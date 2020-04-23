71772172b240d34f03ce6494c052ec59d5321c50
Mario <mariohdpz@gmail.com>
2020-04-23 11:52:30 -0500

    Updates

---
+++ precache-manifest.9730b12c2e252979512d0f3700748b37.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bb856dc30ae8d29c9805a06cf2fd6d",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/index.html"
+  },
+  {
+    "revision": "cb5fbacb68bf740ed865",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/css/main.0fc27154.chunk.css"
+  },
+  {
+    "revision": "61fec742709d656df64a",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/js/2.fba56a9b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/js/2.fba56a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5fbacb68bf740ed865",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/js/main.50e06642.chunk.js"
+  },
+  {
+    "revision": "6eae7c1ea77996b79167",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/js/runtime-main.b8f28878.js"
+  },
+  {
+    "revision": "939ab334715d3830ba4f59ab165255ea",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/media/background.939ab334.jpg"
+  },
+  {
+    "revision": "1d5bbcc08d334d967028338c9858c1ca",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/media/header.1d5bbcc0.jpg"
+  },
+  {
+    "revision": "f264f7ba1e8a3014d89ef3cf6633ddf9",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/media/logo.f264f7ba.png"
+  },
+  {
+    "revision": "c25278f348ec77ec73119e14d797249a",
+    "url": "/mariohdpz.github.io/jeanine-gonzalez-react/static/media/profile.c25278f3.png"
+  }
+]);
